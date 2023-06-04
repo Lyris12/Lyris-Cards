7a246c28310cf9ab60998d9bcaa961b029c344c0
Lyris12 <Lyris89@live.com>
2023-06-04 11:13:03 -0400

    update citations

--- script/c102400034.lua
+++ script/c102400034.lua
@@ -1,4 +1,4 @@
---created by Lyris, art at https://www.1e.com/wp-content/uploads/2017/03/tachyon-pop-culture-energy.png
+--created & coded by Lyris, art at https://www.1e.com/wp-content/uploads/2017/03/tachyon-pop-culture-energy.png
 --フォトンシック・タキオン
 local s,id,o=GetID()
 function s.initial_effect(c)
